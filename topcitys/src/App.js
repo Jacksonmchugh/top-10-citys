b71def75f12516cec9952bc49ee8bdf4fd9f976b
Jackson McHugh <jackson.mchugh@gmail.com>
2022-07-26 11:35:04 -0400

    updating app.js in components

--- topcitys/src/App.js
+++ topcitys/src/App.js
@@ -5,20 +5,49 @@ import Home from './components/Home'
 import City from './components/Cities'
 import { Routes, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div>
-      <header>
-        <Nav />
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/Cities" element={<City />}></Route>
-        </Routes>
-      </main>
-    </div>
-  )
+const App = () => {
+  // The boatsArray is passed into state as the initial state for 'boats' in App.js
+  const [cities, setCities] = useState(citiesArray)
+  const [newCity, setNewCity] = useState({
+    id: '',
+    name: '',
+    img: '',
+    description: '',
+    population: ''
+  })
+
+  const addCity = (e) => {
+    e.preventDefault()
+    const currentCity = cities
+    const createdCity = {
+      ...newCity,
+      id: parseInt(city.length + 1),
+      population: parseInt(newCity.population)
+    }
+    currentCity.push(createdCity)
+    setCity(currentCity)
+    setNewCity({ id: '', name: '', img: '', description: '', population: '' })
+  }
+
+  const handleChange = (e) => {
+    setNewCity({ ...newCity, [e.target.name]: e.target.value })
+  }
+
+  {
+    return (
+      <div>
+        <header>
+          <Nav />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/Cities" element={<City />}></Route>
+          </Routes>
+        </main>
+      </div>
+    )
+  }
 }
 
 export default App
