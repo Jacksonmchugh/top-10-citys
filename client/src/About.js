c217bf229316ce967e5a44a84effac41567850dd
Jackson McHugh <jackson.mchugh@gmail.com>
2022-07-29 11:49:48 -0400

    project 2 done. need more time to get update working and to style.

--- client/src/About.js
+++ client/src/About.js
@@ -5,7 +5,8 @@ const About = () => {
       <h3>
         This site will show you the top 10 cities based on population. If you
         click on a city it will give you some nicknames and attractions for that
-        city. Users will be able to add a city with the city form.
+        city. Users will be able to add a city with the city form. Users can
+        remove a city from the list on the details page.
       </h3>
       <Link to="/">Back</Link>
     </div>
--- client/src/App.js
+++ client/src/App.js
@@ -8,30 +8,6 @@ import Cities from './components/Cities'
 import CityForm from './components/CityForm'
 import CityDetails from './components/CitiesDetails'
 const App = () => {
-  const addCity = (e) => {
-    e.preventDefault()
-    const currentCity = cities
-    const createdCity = {
-      ...newCity,
-      id: parseInt(city.length + 1),
-      population: parseInt(newCity.population)
-    }
-    currentCity.push(createdCity)
-    setCity(currentCity)
-    setNewCity({ id: '', name: '', image: '', description: '', population: '' })
-  }
-
-  const handleChange = (e) => {
-    setNewCity({ ...newCity, [e.target.name]: e.target.value })
-  }
-  // const removeCity = ()=>{
-  //   const city = cities
-  //   if(lastCity.length > 0){
-  //     const lastIndex = city.length - 1
-  //     setCities(city.filter((city, )))
-  //   }
-  // }
-
   return (
     <div className="App">
       <header>
