cfe95a75dfb20c8556a5a049d0b3fa35216c250f
Jackson McHugh <jackson.mchugh@gmail.com>
2022-08-05 17:17:37 -0400

    back to working site. i hope.

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,27 @@
-import { useState, useEffect } from 'react'
+import './App.css'
+import { useState } from 'react'
 import Nav from './components/Nav'
-import Home from './pages/Home'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import Home from './Home'
+import { Route, Routes } from 'react-router-dom'
 import About from './About'
-import Cities from './pages/Cities'
-import CityForm from './pages/CityForm'
-import CityDetails from './pages/CitiesDetails'
-import CommentDetails from './pages/CommentDetails'
-import CommentForm from './pages/CommentForm'
-import axios from 'axios'
+import Cities from './components/Cities'
+import CityForm from './components/CityForm'
+import CityDetails from './components/CitiesDetails'
+import UpdateCity from './components/UpdateCity'
 const App = () => {
-  const [cities, setCities] = useState([])
-  const [selectedCity, setSelectedCity] = useState({})
-
-  useEffect(() => {
-    const getCities = async () => {
-      const res = await axios.get(`localhost:3001/cities`)
-      setCities(res.data)
-      
-    }
-    getCities()
-  }, [])
-  const chooseCity = (selected) => {
-    setSelectedCity(selected)
-    Navigate(`/cities/${selected._id}`)
-  }
   return (
     <div className="App">
       <header>
         <Nav />
       </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Cities" element={<Cities cities={cities} />} />
-          <Route path="/Cities/:id" element={<CityDetails />} />
-          <Route path="/CityForm" element={<CityForm />} />
-          <Route
-            path="/Cities/:cityId/comment/:commentId"
-            element={<CommentDetails />}
-          />
-          <Route path="/Cities/:cityId/comment" element={<CommentForm />} />
-        </Routes>
-      </main>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/About" element={<About />}></Route>
+        <Route path="/Cities" element={<Cities />}></Route>
+        <Route path="/Cities/:id" element={<CityDetails />}></Route>
+        <Route path="/CityForm" element={<CityForm />}></Route>
+        <Route path="/UpdateCity" element={<UpdateCity />}></Route>
+      </Routes>
     </div>
   )
 }
---
+++ client/src/components/CitiesDetails.jsx
@@ -0,0 +1,79 @@
+import { Navigate, useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+
+import axios from 'axios'
+
+const CityDetails = (props) => {
+  
+  const [city, setCity] = useState('')
+  const [formState, setFormState]=useState('')
+
+  let { id }= useParams()
+
+  useEffect(() => {
+    
+    const selectedCity = async()=>{
+      try {
+        let res = await axios.get(`http://localhost:3001/cities/${id}`)
+        console.log(res.data)
+        setCity(res.data)
+      } catch(eer){
+        console.log(eer)
+      }
+    }
+    setCity(selectedCity)
+  },[])
+
+const updateCity = async()=>{
+  console.log(id)
+  let res = await axios.post(`http://localhost:3001/cities/${id}`)
+
+}
+
+
+
+  const deleteCity = async ()=>{
+    console.log(id)
+    let res = await axios.delete(`http://localhost:3001/cities/${id}`,
+    
+    )
+    Navigate('/cities')
+  }
+  
+
+  return city ? (
+    <div className="detail">
+      <div className="detail-header">
+      
+        <img src={city.image} alt={city.image} />
+        <div style={{minWidth: '30em', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <h1>{city.name}</h1>
+        </div> 
+        
+      
+      </div>
+      <div className="info-wrapper">
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+          <h3>Population: {city.population}</h3>
+          <h3>Visits per year: {city.visits}</h3>
+        </div>
+        <p>{city.description}</p>
+          <Link to='/updatecity'>Edit City</Link>
+
+          <button onClick={deleteCity}>Delete City</button>
+          
+        <Link to="/cities">Back</Link>
+        
+      </div>
+      
+    </div>
+    
+  ) : null;
+}
+
+
+
+
+export default CityDetails
